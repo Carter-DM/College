6a5697d14d5cab2ab8bc516b06cb90e1fe296cf9
Carter-DM <cdalymac@gmail.com>
2018-10-03 16:58:16 -0230

    Refactor employee tester

--- src/JavaCP2280/Assignment1/EmployeeTester.java
+++ src/JavaCP2280/Assignment1/EmployeeTester.java
@@ -11,30 +11,28 @@ public class EmployeeTester {
         Employee employee = new Employee("David", "Parsons", 3900.00);
         Employee employee2 = new Employee("Loren", "Brooks", 4650.00);
 
-        printYearlySalary(employee, employee2);
+        printYearlySalary(employee);
+        printYearlySalary(employee2);
 
         System.out.println("Salaries raised by 10%");
         employee.setMonthlySalary(employee.getMonthlySalary() * 1.1);
         employee2.setMonthlySalary(employee2.getMonthlySalary() * 1.1);
 
-        printYearlySalary(employee, employee2);
+        printYearlySalary(employee);
+        printYearlySalary(employee2);
 
     }
 
     /**
      * printYearlySalary
-     * <p>Prints yearly salary of both tester employees</p>
+     * <p>Prints yearly salary of both tester employee</p>
      *
      * @param employee
-     * @param employee2
      */
-    private static void printYearlySalary(Employee employee, Employee employee2) {
-        System.out.printf("Yearly salary for %s %s: $%.2f\nYearly salary for %s %s: $%.2f\n",
+    private static void printYearlySalary(Employee employee) {
+        System.out.printf("Yearly salary for %s %s: $%.2f\n",
                 employee.getFirstName(),
                 employee.getLastName(),
-                employee.getMonthlySalary() * 12,
-                employee2.getFirstName(),
-                employee2.getLastName(),
-                employee2.getMonthlySalary() * 12);
+                employee.getMonthlySalary() * 12);
     }
 }
--- src/JavaCP2280/Assignment1/InvoiceTester.java
+++ src/JavaCP2280/Assignment1/InvoiceTester.java
@@ -8,7 +8,7 @@ package JavaCP2280.Assignment1;
 public class InvoiceTester {
 
     public static void main(String[] args) {
-        Invoice invoice = new Invoice("10827", "30in x 2in PVC Pipe", -20, 7.50);
+        Invoice invoice = new Invoice("10827", "30in x 2in PVC Pipe", -20, -7.50);
         System.out.printf("Part Number: %s\nPart Description: %s\nItem Quantity: %d\nItem Price: %.2f\n\n",
                 invoice.getPartNumber(),
                 invoice.getPartDescription(),
