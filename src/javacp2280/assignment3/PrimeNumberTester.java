7e0dea3dd787a2273c0a963f33aa9843ef060613
Carter-DM <cdalymac@gmail.com>
2018-11-01 16:37:00 -0230

    Expand testing interface

--- src/javacp2280/assignment3/PrimeNumberTester.java
+++ src/javacp2280/assignment3/PrimeNumberTester.java
@@ -3,40 +3,81 @@ package javacp2280.assignment3;
 import java.util.Scanner;
 
 /**
- *Tester class for Prime Number Calculator
+ * Tester class for Prime Number Calculator
  *
  * @author Carter Daly-MacPhail
  */
 public class PrimeNumberTester {
 
     public static void main(String[] args) {
-        System.out.println("Should be true: "+PrimeNumberCalculator.isPrime(5));
-        System.out.println("Should be true: "+PrimeNumberCalculator.isPrime(2));
-        System.out.println("Should be true: "+PrimeNumberCalculator.isPrime(3));
-        System.out.println("Should be true: "+PrimeNumberCalculator.isPrime(7));
-        System.out.println("Should be false: "+PrimeNumberCalculator.isPrime(1));
-        System.out.println("Should be false: "+PrimeNumberCalculator.isPrime(10));
-        System.out.println("Should be false: "+PrimeNumberCalculator.isPrime(9));
-        System.out.println("Should be true: "+PrimeNumberCalculator.isPrime(19));
-        System.out.println("Should be false: "+PrimeNumberCalculator.isPrime(240));
-
-        System.out.println(PrimeNumberCalculator.getUniquePrimeFactorization(25));
-
-
-        System.out.println(PrimeNumberCalculator.printPrimesBetweenNumbers(0, 10000));
 
         Scanner in = new Scanner(System.in);
         String userInput = "";
         do {
-            System.out.println("\n\n=======================================================");
-            System.out.println("Enter the corresponding key below to access one" +
-                    " of the following options:\n" +
-                    "[1] Determine if a number is prime.\n" +
-                    "[2] Get the unique prime factorization of a number.\n" +
-                    "[3] Print all prime numbers between two numbers.\n" +
-                    "[4] Print all prime numbers up to 10,000.\n" +
-                    "[x] Quit");
+            System.out.println("\n=======================================================");
+            printMainMenu();
             userInput = in.next();
-        }while(!userInput.equalsIgnoreCase("x"));
+
+            if (userInput.equals("1")) {
+                selectedIsPrime(in);
+            } else if (userInput.equals("2")) {
+                selectedUniquePrimeFactorization(in);
+            } else if (userInput.equals("3")) {
+                selectedGetPrimesBetweenTwoNumbers(in);
+            } else if (userInput.equals("4")) {
+                selectedPrintPrimesUpTo10000();
+            } else {
+                System.out.println("Invalid input.");
+            }
+
+        } while (!userInput.equalsIgnoreCase("x"));
+    }
+
+    private static void printMainMenu() {
+        System.out.println("Enter the corresponding key below to access one" +
+                " of the following options:\n" +
+                "[1] Determine if a number is prime.\n" +
+                "[2] Get the unique prime factorization of a number.\n" +
+                "[3] Print all prime numbers between two numbers.\n" +
+                "[4] Print all prime numbers up to 10,000.\n" +
+                "[x] Quit");
+    }
+
+    private static void selectedIsPrime(Scanner in) {
+        System.out.println("===================\n" +
+                "IS NUMBER A PRIME?\n" +
+                "===================\n" +
+                "Enter an integer:");
+        if (PrimeNumberCalculator.isPrime(in.nextInt())) {
+            System.out.println("Yes, the number you entered is prime!");
+        } else {
+            System.out.println("No, the number you entered is not prime.");
+        }
+    }
+
+    private static void selectedUniquePrimeFactorization(Scanner in) {
+        System.out.println("===================\n" +
+                "GET PRIME FACTORS\n" +
+                "===================\n" +
+                "Enter an integer:");
+        System.out.println(PrimeNumberCalculator.getUniquePrimeFactorization(in.nextInt()));
+    }
+
+    private static void selectedGetPrimesBetweenTwoNumbers(Scanner in) {
+        System.out.println("===================\n" +
+                "GET PRIMES BETWEEN BOUNDS\n" +
+                "===================\n" +
+                "Enter lower bound (integer):");
+        int lowerBound = in.nextInt();
+        System.out.println("Enter upper bound (integer):");
+        int upperBound = in.nextInt();
+        System.out.println(PrimeNumberCalculator.printPrimesBetweenNumbers(lowerBound, upperBound));
+    }
+
+    private static void selectedPrintPrimesUpTo10000() {
+        System.out.println("===================\n" +
+                "PRIMES UP TO 10,000\n" +
+                "===================\n");
+        System.out.println(PrimeNumberCalculator.printPrimesBetweenNumbers(0, 10000));
     }
 }
