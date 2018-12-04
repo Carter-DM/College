5369fc5c73870ce6f00236caca91427a7a4f8b74
Carter-DM <cdalymac@gmail.com>
2018-12-04 20:18:59 -0330

    Added health insurance calculations

--- src/javacp2280/Project/EmployeeHealthInsuranceCalculator.java
+++ src/javacp2280/Project/EmployeeHealthInsuranceCalculator.java
@@ -6,9 +6,22 @@ package javacp2280.Project;
  * @author Carter Daly-MacPhail
  */
 public class EmployeeHealthInsuranceCalculator {
+    public static final double BMI_OVER_25 = .75;
+    public static final double BMI_OVER_30 = 1.5;
+    public static final double EXCEEDED_HEART_RATE_LIMIT = .5;
 
-    public static double getAnnualHealthInsuranceCost(double salary, double BMI, double heartRate, double maxHeartRate){
-
+    public static double getAnnualHealthInsuranceCost(double salary, double BMI, double heartRate, double maxHeartRate) {
+        double annualInsurance = 0;
+        annualInsurance += salary * .01;
+        if (BMI > 30) {
+            annualInsurance += annualInsurance * BMI_OVER_30;
+        } else if (BMI > 25) {
+            annualInsurance += annualInsurance * BMI_OVER_25;
+        }
+        if (heartRate > maxHeartRate){
+            annualInsurance += annualInsurance * EXCEEDED_HEART_RATE_LIMIT;
+        }
+        return annualInsurance;
     }
 
 }
