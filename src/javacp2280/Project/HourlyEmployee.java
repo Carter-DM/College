9369a06699ab742f75dce415e408ba9748ff3562
Carter-DM <cdalymac@gmail.com>
2018-12-03 21:24:32 -0330

    hoursPerWeek variable

--- src/javacp2280/Project/HourlyEmployee.java
+++ src/javacp2280/Project/HourlyEmployee.java
@@ -2,6 +2,7 @@ package javacp2280.Project;
 
 public class HourlyEmployee extends Employee {
     private double hourlyWage;
+    private double hoursPerWeek;
 
     /**
      * Person Constructor
@@ -14,14 +15,16 @@ public class HourlyEmployee extends Employee {
      * @param height     Persons height in inches
      * @param weight     Persons weight in pounds
      * @param hourlyWage Employees hourly wage
+     * @param hoursPerWeek Employees hours worked per week
      */
-    public HourlyEmployee(String fName, String lName, int dobDay, int dobMonth, int dobYear, double height, double weight, double hourlyWage) {
+    public HourlyEmployee(String fName, String lName, int dobDay, int dobMonth, int dobYear, double height, double weight, double hourlyWage, double hoursPerWeek) {
         super(fName, lName, dobDay, dobMonth, dobYear, height, weight);
         this.hourlyWage = hourlyWage;
+        this.hoursPerWeek = hoursPerWeek;
     }
 
     @Override
     public double getAnnualWage() {
-        return this.hourlyWage * 40 * 52;
+        return this.hourlyWage * this.hoursPerWeek * 52;
     }
 }
