d0d8bec2dd594266fb2b1c1a6b39e353b79c0305
Carter-DM <cdalymac@gmail.com>
2020-02-21 14:08:48 -0330

    LibraryFactory init commit

--- src/javacp3561/assignment3/LibraryFactory.java
+++ src/javacp3561/assignment3/LibraryFactory.java
@@ -1,5 +1,33 @@
 package javacp3561.assignment3;
 
-public class LibraryFactory {
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * LibraryFactory class.
+ * Takes path to XML doc and parses it into the relevant objects.
+ */
+public class LibraryFactory {
+    DocumentBuilderFactory factory;
+    DocumentBuilder builder;
+
+    public LibraryFactory(String pathname) {
+        factory = DocumentBuilderFactory.newInstance();
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            Document document = builder.parse(new File(pathname));
+        } catch (SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
